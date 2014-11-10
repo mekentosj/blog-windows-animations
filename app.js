da7500655bf401c7836bfb8045e830df98689018
Ericat <erica.salvaneschi@hotmail.com>
2014-11-10 15:10:11 +0000

    Added platform-specific logos

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ var app = express();
 var path = require('path');
 var routes = require('./routes');
 
+function setPage(page) {
+  return function(req, res, next) {
+    res.locals.page = page;
+    next();
+  };
+}
+
+
 app.set('env', process.env.NODE_ENV || 'development');
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
@@ -10,6 +18,9 @@ app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', routes.index);
-app.get('/papers', routes.papers);
+app.get('/windows', setPage('windows'), routes.windows);
+app.get('/mac', setPage('mac'), routes.mac);
+app.get('/ios', setPage('ios'), routes.ios);
+app.get('/online', setPage('online'), routes.online);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,18 @@ module.exports.index = function(req, res, next) {
   res.render('index');
 }
 
-module.exports.papers = function(req, res, next) {
-  res.render('papers');
+module.exports.windows = function(req, res, next) {
+  res.render('index');
+}
+
+module.exports.mac = function(req, res, next) {
+  res.render('index');
+}
+
+module.exports.ios = function(req, res, next) {
+  res.render('index');
+}
+
+module.exports.online = function(req, res, next) {
+  res.render('index');
 }
