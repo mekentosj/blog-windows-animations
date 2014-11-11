8903e69ffd614e33ff1db87ff250ae7c4304fe40
Ericat <erica.salvaneschi@hotmail.com>
2014-11-11 17:00:28 +0000

    Now running on port 5454

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var app = require('./app');
 var http = require('http');
 
-http.createServer(app).listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + app.get('port'));
-})
+http.createServer(app).listen('5454'), function() {
+  console.log('Express server listening on port 5454');
+}
